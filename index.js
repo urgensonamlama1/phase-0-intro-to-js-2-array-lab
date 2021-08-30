683c1212a4692594d554981c09334dab4993ad0a
urgensonamlama <urgensonamlama@gmail.com>
2021-08-30 15:12:23 -0400

    Incompleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats= ["Milo","Otis","Garfield"];
+
+
+/*
+
+cats.push("Ralph");
+
+cats.unshift("Bob");
+
+cats.pop();
+
+cats.shift();
+
+const newCats = [
+    ...cats.slice(0,0),
+    "Arnlod",
+    ...cats.slice(0),
+];
+
+
+const newCats = [
+    ...cats.slice(0,0),
+    "Arnlod",
+    ...cats.slice(0),
+];
+
+const newCats = [
+    ...cats.slice(0,2),
+   
+];
+
+const newCats = [
+    ...cats.slice(1),
+   
+];
+
+*/
