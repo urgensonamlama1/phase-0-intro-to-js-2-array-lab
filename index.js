1f6c1da6bc96a8f4f6604a139ab613ea2d768bd8
urgensonamlama <urgensonamlama@gmail.com>
2021-08-30 15:54:00 -0400

    completed assignment

--- index.js
+++ index.js
@@ -2,37 +2,73 @@
 const cats= ["Milo","Otis","Garfield"];
 
 
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+};
+
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+
+};
+
+function  appendCat(name){
+    const newCats = [...cats]
+    newCats.push(name);
+    return newCats;
+
+}
+
 /*
-
-cats.push("Ralph");
-
-cats.unshift("Bob");
-
-cats.pop();
-
-cats.shift();
-
-const newCats = [
-    ...cats.slice(0,0),
-    "Arnlod",
-    ...cats.slice(0),
-];
-
-
-const newCats = [
-    ...cats.slice(0,0),
-    "Arnlod",
-    ...cats.slice(0),
-];
-
-const newCats = [
-    ...cats.slice(0,2),
+function prependCat(name){
+    const newCats = [
+        ...cats.slice(0,0),
+        "Arnlod",
+        ...cats.slice(0),
    
-];
-
-const newCats = [
-    ...cats.slice(1),
-   
-];
-
+    ];
+    
+    return newCats;
+}
 */
+
+function prependCat(name){
+    return [name,...cats];
+
+}
+
+
+function removeLastCat(){
+    const newCats = [
+        ...cats.slice(0,2),  
+    ];
+    return newCats;
+    
+}
+
+function  removeFirstCat(){
+    const newCats = [
+        ...cats.slice(1),
+       
+    ];
+    return newCats;
+
+
+}
+
+
+
+
+
+
+
+
+
